89f76ceafa50bf5f5eee2c12f8285a36fc3c5f10
Stoned Elipot <stoned.elipot@gmail.com>
2016-04-10 08:48:56 +0200

    Use Puppet and Chef packages with Ruby 2.1

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -13,15 +13,13 @@ esac
 #
 case ":$PROVISIONER:" in
 *:chef:*)
-  pkgin -y install ruby193-chef
+  pkgin -y install ruby21-chef
 ;;
 esac
 
 # 
 case ":$PROVISIONER:" in
 *:puppet:*)
-  # we do not install puppet with ruby 2.1.2 (ruby212-puppet package)
-  # as a workaround for https://tickets.puppetlabs.com/browse/PUP-1243
-  pkgin -y install ruby193-puppet
+  pkgin -y install ruby21-puppet
 ;;
 esac
